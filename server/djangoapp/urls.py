e39c635d27e5f0a6650530c23175af87f79e527b
0x10119 <longnq.it@gmail.com>
2023-04-27 23:01:39 -0400

    Task 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path(route='logout/', view=views.logout_request, name='logout'),
     
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
 
